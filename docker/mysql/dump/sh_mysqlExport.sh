69b34bbfc2c7c11b07042960effabfc4b562659e
shibata_ko <shibata_ko@worksap.co.jp>
2014-09-12 12:26:31 +0900

    dbDump

--- docker/mysql/dump/sh_mysqlExport.sh
+++ docker/mysql/dump/sh_mysqlExport.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mysqldump -h 192.168.33.33 -u root -proot -A > /media/shiba/backup/develop/dbDump/prm1-mysql/`date +%Y%m%d%H%M`$1.sql
+mysqldump -h 192.168.33.33 -u root -proot -A > ./`date +%Y%m%d%H%M`$1.sql
 
